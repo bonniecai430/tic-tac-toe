f40a787a496e70a4373e78ed65bd83ebe88ef09e
bonnie430 <bonniecai97@gmail.com>
2022-05-31 22:55:08 -0700

    update2

--- js/main.js
+++ js/main.js
@@ -1,3 +1,18 @@
+const sq = document.querySelectorAll('.sq')
+const PLAYER_X = "X"
+const PLAYER_O = "O"
+let turn =PLAYER_X
+
+const boardState = Array(sq.length);
+boardState.fill(null)
+// Elemnent
+
+
+
+
+
+
+
 /*----- constants -----*/
 /*----- app's state (variables) -----*/
 /*----- cached element references -----*/
